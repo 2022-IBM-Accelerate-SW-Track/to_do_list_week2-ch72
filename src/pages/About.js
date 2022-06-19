2b2b164ff5116d52d9964f32201c0cf810517ea3
ch72 <chong2744sdsu@gmail.com>
2022-06-19 15:31:27 -0700

    Personalized "About Me" page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,35 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import profile_pic from "../assets/pfp.png";
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+        <p>
+          <div>
+            <div class="split left">
+              <div className="centered">
+                <img 
+                  className="profile_image"
+                  src={profile_pic}
+                  alt="Profile Pic"
+                  ></img>
+              </div>
+            </div>
+            <div className="split right">
+              <div className="centered">
+                <div className="name_title">Christian Hong</div>
+                <div className="brief_description">
+                  I'm a rising senior at San Diego State University whose interested 
+                  in pursuing full stack or cloud engineering. Feel free to reach out to me 
+                  on <a href="https://www.linkedin.com/in/christian-hong/">LinkedIn</a>.
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        </p> 
       </div>
-    );
+    )
   }
 }
